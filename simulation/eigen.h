e3d5cdcd172b11d1c50c6391b5d661b2c1b724fa
dchandran1 <dchandran1@24e2686d-6f0d-4b86-b305-2fc710cdb7e5>
2009-10-30 20:53:44 +0000

    * reverted to using simulation to calculate steadystate instead of neldermead * added eigenvalue calculation to jacobian function in cvodeim

---
+++ simulation/eigen.h
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include "f2c.h"
+#include "blaswrap.h"
+#include "clapack.h"
+
+/* finds the eigenvalues of a matrix using CLAPACK
+ * \param: square matrix
+ * \param: matrix dimension
+ * \param: (output) vector of real values
+ * \param: (output) vector of imaginary values
+ * \return: 0 = failure 1 = success
+*/
+int eigenvalues(double * A, int n, double * reals, double * im);
+
+
+#define getValue(array, N, i, j) ( array[ i*(N) + j ] )
